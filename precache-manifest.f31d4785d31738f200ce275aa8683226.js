3100de63992afcdd4e977509537c08032d74b8fe
ndmen <ndmen@users.noreply.github.com>
2022-08-12 15:09:55 +0000

    deploy: 803ba79cf85b72359f2d4a146174f033fee12976

---
+++ precache-manifest.f31d4785d31738f200ce275aa8683226.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdc387e705ab1df0d19d8d1a5c731c9",
+    "url": "https://ndmen.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "7fe757f92c3cffc67bd2",
+    "url": "https://ndmen.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "6edfc617707965a4680d",
+    "url": "https://ndmen.github.io/task-management-frontend/static/js/2.a315a08f.chunk.js"
+  },
+  {
+    "revision": "7fe757f92c3cffc67bd2",
+    "url": "https://ndmen.github.io/task-management-frontend/static/js/main.bb36ac6d.chunk.js"
+  },
+  {
+    "revision": "d7aa18952e689394ba1a",
+    "url": "https://ndmen.github.io/task-management-frontend/static/js/runtime~main.4a2e909d.js"
+  }
+]);
